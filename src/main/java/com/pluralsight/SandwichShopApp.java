993ab70b4831ff2c478980aad6ffd9bb73df9c93
Yumozh <juliiamozhchil@gmail.com>
2026-04-10 14:04:07 -0500

    feat: proceed discount based on age

--- src/main/java/com/pluralsight/SandwichShopApp.java
+++ src/main/java/com/pluralsight/SandwichShopApp.java
@@ -7,19 +7,35 @@ public class SandwichShopApp {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter a size number of the sandwich (1) Regular and (2) Large : ");
-        int sandwichSize  = scanner.nextInt();
+        int sandwichSize = scanner.nextInt();
 
         System.out.println("Please enter your age: ");
-        int age  = scanner.nextInt();
+        int age = scanner.nextInt();
 
         double price;
 
-        if (sandwichSize == 1){
+        if (sandwichSize == 1) {
             price = 5.45;
-        } else{
+        } else {
             price = 8.95;
         }
 
+        double costFinal;
 
+        if (age <= 17) {
+
+            costFinal = price * 0.1;
+        }
+        else if (age >= 65) {
+
+            costFinal = price * 0.2;
+        }
+        else{
+
+            costFinal = price;
+
+        }
+
+        System.out.println("Your total cost will be: $" + costFinal);
     }
 }
