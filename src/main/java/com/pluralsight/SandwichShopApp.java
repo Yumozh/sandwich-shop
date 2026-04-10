d5c44c9fdfa230f01585d9b37607dba7802df906
Yumozh <juliiamozhchil@gmail.com>
2026-04-10 14:25:51 -0500

    feat: calculate price if sandwich is loaded

--- src/main/java/com/pluralsight/SandwichShopApp.java
+++ src/main/java/com/pluralsight/SandwichShopApp.java
@@ -11,13 +11,23 @@ public class SandwichShopApp {
 
         System.out.println("Please enter your age: ");
         int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Tell yes/no if you want your sandwich loaded : ");
+        String isLoaded = scanner.nextLine();
 
         double price;
 
         if (sandwichSize == 1) {
             price = 5.45;
+            if (isLoaded.equals("yes")){
+                price = 6.45;
+            }
         } else {
             price = 8.95;
+            if(isLoaded.equals("yes")){
+               price = 10.7;
+            }
         }
 
         double costFinal;
@@ -25,17 +35,15 @@ public class SandwichShopApp {
         if (age <= 17) {
 
             costFinal = price - (price * 0.1);
-        }
-        else if (age >= 65) {
+        } else if (age >= 65) {
 
             costFinal = price - (price * 0.2);
-        }
-        else{
+        } else {
 
             costFinal = price;
 
         }
 
-        System.out.printf("Your total cost will be: $%.2f",costFinal);
+        System.out.printf("Your total cost will be: $%.2f", costFinal);
     }
 }
