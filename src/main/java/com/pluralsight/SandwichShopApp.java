549d76563b2e82f29febdbd82d06f0669ec70fbb
Yumozh <juliiamozhchil@gmail.com>
2026-04-10 11:34:07 -0500

    feat: identify a price on sandwich

--- src/main/java/com/pluralsight/SandwichShopApp.java
+++ src/main/java/com/pluralsight/SandwichShopApp.java
@@ -7,5 +7,19 @@ public class SandwichShopApp {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter a size number of the sandwich (1) Regular and (2) Large : ");
+        int sandwichSize  = scanner.nextInt();
+
+        System.out.println("Please enter your age: ");
+        int age  = scanner.nextInt();
+
+        double price;
+
+        if (sandwichSize == 1){
+            price = 5.45;
+        } else{
+            price = 8.95;
+        }
+
+
     }
 }
