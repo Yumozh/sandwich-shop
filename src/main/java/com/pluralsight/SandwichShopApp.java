5ede2016ce6b55b245983344f0590887a90328d1
Yumozh <juliiamozhchil@gmail.com>
2026-04-10 11:28:32 -0500

    Request user input

--- src/main/java/com/pluralsight/SandwichShopApp.java
+++ src/main/java/com/pluralsight/SandwichShopApp.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class SandwichShopApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a size number of the sandwich (1) Regular and (2) Large : ");
+    }
 }
